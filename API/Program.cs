78a9f6c59144f3e34e1b195b08285486f9cf3b60
Sevdariel <michal.zalewski1995@gmail.com>
2023-08-05 01:05:14 +0200

    Create Angular project

--- API/Program.cs
+++ API/Program.cs
@@ -10,8 +10,11 @@ builder.Services.AddDbContext<DataContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors();
+
 var app = builder.Build();
 
+app.UseCors(corsPolicyBuilder => corsPolicyBuilder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
---
+++ Client/src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent implements OnInit {
+
+  title = 'Client';
+  users: any;
+
+  constructor(private httpClient: HttpClient) { }
+
+  public ngOnInit(): void {
+    this.httpClient.get('https://localhost:5001/api/users')
+      .subscribe({
+        next: response => this.users = response,
+        error: error => console.log(error),
+        complete: () => console.log('Request has been completed'),
+      });
+  }
+}
