c2e79cea661b57bbcb3f3049912a5bcfaf7eb953
Sevdariel <michal.zalewski1995@gmail.com>
2023-08-04 18:50:32 +0200

    Create Db
    Add DbContext
    Add first entity
    Add defaultConnection

--- API/Program.cs
+++ API/Program.cs
@@ -1,6 +1,22 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
 
 app.Run();
+
