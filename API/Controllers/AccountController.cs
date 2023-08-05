9129bb9fed72998971eae91c262c30c1e5177a2f
Michał Zalewski <michal.zalewski1995@gmail.com>
2023-08-05 22:45:45 +0200

    User update
    Extend User entity
    Add AccountController
    Add RegisterDto

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,48 @@
+﻿using API.Data;
+using API.Dto;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace API.Controllers
+{
+    public class AccountController : BaseApiController
+    {
+        private readonly DataContext dataContext;
+
+        public AccountController(DataContext dataContext)
+        {
+            this.dataContext = dataContext;
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<User>> Register(RegisterDto registerDto)
+        {
+            if (await UserExists(registerDto.Username))
+                return BadRequest("Username is taken");
+
+            using var hmac = new HMACSHA512();
+
+            var user = new User
+            {
+                Username = registerDto.Username.ToLower(),
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
+                PasswordSalt = hmac.Key,
+            };
+
+            dataContext.Users.Add(user);
+
+            await dataContext.SaveChangesAsync();
+
+            return user;
+        }
+
+        
+        private async Task<bool> UserExists(string username)
+        {
+            return await dataContext.Users.AnyAsync(user => user.Username == username.ToLower());
+        }
+    }
+}
